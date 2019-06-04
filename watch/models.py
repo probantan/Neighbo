fbbae75f662555ca3a6bf081bea9570a85b7bd75
protus <protus.bantan@gmail.com>
2019-06-04 15:45:55 +0300

    created my commentform

--- watch/models.py
+++ watch/models.py
@@ -103,25 +103,3 @@ class Post(models.Model):
         posts = Post.objects.filter(id = id)
         return posts
 
-class Comment(models.Model):
-    name = models.CharField(max_length=30)
-    post = models.ForeignKey(Post,null = True)
-    neighborhood = models.ForeignKey(NeighborHood,related_name='comment')
-
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        ordering = ['-id']
-
-    def save_comment(self):
-        self.save()
-
-    def delete_comment(self):
-        self.delete()
-
-    @classmethod
-    def find_commentpost(cls,id):
-        comments = Comments.objects.filter(post__pk = id)
-        return comments
