a4294adf1e5a673f023bc06572718139e73b4c34
protus <protus.bantan@gmail.com>
2019-06-04 16:17:39 +0300

    implemented such functionalit

--- watch/views.py
+++ watch/views.py
@@ -94,6 +94,8 @@ def one_post(request,post_id):
         return render(request, 'commentspace.html', locals())
     return redirect('hood')
 
+
+
 @login_required(login_url='/accounts/login')
 def add_post(request):
     hoo = NeighborHood.objects.get(id=request.user.profile.neighborhood.id)
@@ -110,3 +112,13 @@ def add_post(request):
         postform = PostForm()
     return render(request,'add-post.html',locals())
 
+@login_required(login_url='/accounts/login')
+def search_results(request):
+    business= Business.objects.all()
+    hood = NeighborHood.objects.get(id=request.user.profile.neighborhood.id)
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_business = Business.search(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',locals())
